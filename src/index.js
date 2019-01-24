095201410f535b39c7e121b0bf8a98e2cede9df9
Nicolas D <nicolas_delperdange@hotmail.com>
2019-01-24 14:56:32 +0100

    feat(app): upgrading material-ui-pickers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { addField, FieldTitle } from 'ra-core';
-import { DatePicker, TimePicker, DateTimePicker } from 'material-ui-pickers';
-import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils';
-import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, DatePicker, TimePicker, DateTimePicker } from 'material-ui-pickers';
 
 const makePicker = (PickerComponent) => {
   class _makePicker extends Component {

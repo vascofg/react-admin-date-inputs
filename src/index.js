fd0ccb7951db263ff47d28d7e1837e7a86a9dd2e
david <david@simplyphp.com>
2020-07-30 10:35:02 -0400

    Fixing issue, bad props name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { DatePicker, DateRangePicker, DateTimePicker, TimePicker } from '@materi
 import RangePicker from './RangePicker';
 import Picker from './Picker';
 
-export const DateInput = props => <Picker PickerComponent={ DatePicker } { ...props } />
-export const TimeInput = props => <Picker PickerComponent={ TimePicker } { ...props } />
-export const DateTimeInput = props => <Picker PickerComponent={ DateTimePicker } { ...props } />
-export const DateRangeInput = props => <RangePicker PickerComponent={ DateRangePicker } { ...props } />
+export const DateInput = props => <Picker Component={ DatePicker } { ...props } />
+export const TimeInput = props => <Picker Component={ TimePicker } { ...props } />
+export const DateTimeInput = props => <Picker Component={ DateTimePicker } { ...props } />
+export const DateRangeInput = props => <RangePicker Component={ DateRangePicker } { ...props } />
